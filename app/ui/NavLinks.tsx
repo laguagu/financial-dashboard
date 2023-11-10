29621a49be488f95af27794627bb7eccc979ea1b
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-10 20:40:40 +0200

    new logos align items center

--- app/ui/NavLinks.tsx
+++ app/ui/NavLinks.tsx
@@ -28,9 +28,9 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={classNames(
-              "bg-gray-100 p-3 flex hover:text-blue-600 hover:font-semibold rounded-md space-x-2 mb-1 transition-colors duration-300",
+              "bg-gray-100 p-3 flex hover:text-blue-600 hover:font-semibold rounded-md space-x-3 mb-1 transition-colors duration-300",
               {
-                "bg-sky-100 text-blue-500": link.href === currenPath,
+                "bg-sky-100 text-blue-600": link.href === currenPath,
                 "text-zinc-400": link.href !== currenPath,
                 "transition-colors": true,
               }
--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import React from "react";
 import NavLinks from "./NavLinks";
-import { BanknotesIcon } from "@heroicons/react/20/solid";
 import { PowerIcon } from "@heroicons/react/24/outline";
+import { CreditCardIcon } from "@heroicons/react/20/solid";
 
 export default function Sidenav() {
   return (
     <nav className="flex flex-col px-3 py-4 text-zinc-400 w-44 bg-slate-50 min-h-screen">
-      <Link href="/" className="mb-2 flex items-end rounded-md  p-4 md:h-28 bg-blue-">
-        <BanknotesIcon className="h-14 w-14 mb-3 text-blue-400" />
+      <Link href="/" className="mb-2 flex rounded-md  p-4 md:h-28 bg-zinc-100 items-center justify-center ">
+        <CreditCardIcon className="h-20 w-20 mb-3 text-blue-400 mt-10"  />
       </Link>
       <NavLinks />
       <div className="mt-auto whitespace-nowrap">
