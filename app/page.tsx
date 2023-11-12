71ff7e6c8b797c2630338f64e8fa6b4fb3dee2da
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-12 11:55:52 +0200

    TS improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
 import Sidenav from "./ui/Sidenav";
 import { fetchUsersFromDatabase, saveUsersToDatabase } from "./db/data";
-
-interface User {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string | null;
-}
+import { User } from "@prisma/client";
 
 async function fetchUsersApi(): Promise<User[]> {
   const response = await fetch("https://6549f6b1e182221f8d523a44.mockapi.io/api/Users", {cache: "no-store"});
-  const data: User[] = await response.json();
+  const data = await response.json();
   return data;
 }
 
 export default async function Home() {
-  const apiUsers = await fetchUsersApi()
+  const apiUsers: User[] = await fetchUsersApi()
   await saveUsersToDatabase(apiUsers)
 
   const users = await fetchUsersFromDatabase();
@@ -24,7 +18,7 @@ export default async function Home() {
       <Sidenav />
       <div className="flex-grow flex justify-center items-center">
         <ul>
-          {users.map((user: any) => (
+          {users.map((user: User) => (
             <li key={user.id}>{user.name}</li>
           ))}
         </ul>
