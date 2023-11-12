bc50cb692b75af34547b38424264520f898f4dd9
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-12 11:02:41 +0200

    Prisma and database initialization

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
-import Sidenav from "./ui/sidenav";
+import Sidenav from "./ui/Sidenav";
+import prisma from "@/prisma/db";
 
 async function fetchUsers() {
-  const response = await fetch(
-    "https://6549f6b1e182221f8d523a44.mockapi.io/api/Users"
-  );
+  const response = await fetch("https://6549f6b1e182221f8d523a44.mockapi.io/api/Users");
   const data = await response.json();
   return data;
 }
 
+async function addUsers(user) {
+  const user = await prisma.user.create({
+    data: {
+      id:"2",
+      name:"Matti",
+      email:"Matti@hotmail.com",
+      image_url:"ulr.example.com"
+    }
+  })
+}
+
 export default async function Home() {
   const users = await fetchUsers();
-  console.log("Käyttäjät:", users);
+  await addUsers()
   return (
     <div className="flex">
       <Sidenav />
