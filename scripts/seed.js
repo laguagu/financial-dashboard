a6ef2386133d8e9104053605275b6acd3014a0e0
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-13 12:20:10 +0200

    Seed scripts for invoices

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,28 +1,58 @@
 // import { db } from '@vercel/postgres';
-const { db } = require('@vercel/postgres');
-const {
-  invoices,
-  customers,
-  revenue,
-  users,
-} = require('../app/lib/placeholder-data.js');
+const { db } = require("@vercel/postgres");
+const { invoices } = require("../app/lib/placeholder-data.js");
 
+async function seedInvoices(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    // Create the "invoices" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS invoices (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      customer_id VARCHAR(255) NOT NULL, 
+      amount INT NOT NULL,
+      status VARCHAR(255) NOT NULL,
+      date DATE NOT NULL
+    );
+  `;
+    console.log(`Created "invoices" table`);
+    // Insert data into the "invoices" table
+    const insertedInvoices = await Promise.all(
+      invoices.map(
+        (invoice) => client.sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+          ON CONFLICT (id) DO NOTHING;
+        `
+      )
+    );
+
+    console.log(`Seeded ${insertedInvoices.length} invoices`);
+    return {
+      createTable,
+      invoices: insertedInvoices,
+    };
+  } catch (error) {
+    console.error("Error seeding invoices:", error);
+    throw error;
+  }
+}
 
 async function main() {
   const client = await db.connect();
-  console.log(client)
-  
-//   await seedUsers(client);
-//   await seedCustomers(client);
-//   await seedInvoices(client);
-//   await seedRevenue(client);
 
-//   await client.end();
+  await seedInvoices(client);
+  //   await seedUsers(client);
+  //   await seedCustomers(client);
+  //   await seedRevenue(client);
+
+  //   await client.end();
 }
 
 main().catch((err) => {
   console.error(
-    'An error occurred while attempting to seed the database:',
-    err,
+    "An error occurred while attempting to seed the database:",
+    err
   );
 });
