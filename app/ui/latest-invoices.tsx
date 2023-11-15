d0f83e620b9beb18c4d21d4ef4131b3775e0087c
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-15 16:43:19 +0200

    Customer pictures and some styling

--- app/ui/latest-invoices.tsx
+++ app/ui/latest-invoices.tsx
@@ -8,22 +8,23 @@ export default async function LatestInvoices() {
   return (
     <div>
       <h2 className="mb-4 text-xl">Latest Invoices</h2>
-      <ul className="border bg-white px-2">
+      <div className="border bg-white px-2">
         {invoices.map((invoice) => {
           return (
-        <div key={invoice.id} className="flex flex-row items-center justify-between font-medium">
+        <div key={invoice.id} className="flex flex-row  items-center justify-between font-medium pt-3 border">
             {invoice.amount}
             <div>
                 <Image
-                src={"/customers/image_url 2.png"}
-                alt={`${invoice.name} profile picture`}
+                src={"/customers/happyCustomer.png"}
+                alt={`${invoice.email} profile picture`}
+                className="rounded-full"
                 width={32}
                 height={32}
                 />
             </div>
         </div>
           )})}
-      </ul>
+      </div>
     </div>
   );
 }
