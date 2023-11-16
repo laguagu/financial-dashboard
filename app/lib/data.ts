efcda28a1be7b1077f664c7f2754821689320540
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-16 10:45:10 +0200

    Invoice fetching query

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,6 +5,8 @@ import { sql } from "@vercel/postgres";
 import { LatestInvoice, LatestInvoiceRaw } from "./definitions";
 import { formatCurrency } from "./utils";
 
+// applied SQL query Logics from https://nextjs.org/learn tutorial 
+
 export async function saveCustomerToDatabase(
   customers: Customers[]
 ): Promise<void> {
@@ -51,5 +53,38 @@ export async function fetchLatestInvoices() {
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch the latest invoices.");
+  }
+}
+
+export async function fetchCardData() {
+  try {
+    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+    const customerCountPromise = sql`SELECT COUNT(*) FROM "Customers"`;
+    const invoiceStatusPromise = sql`SELECT
+         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+         FROM invoices`;
+
+    const data = await Promise.all([
+      invoiceCountPromise,
+      customerCountPromise,
+      invoiceStatusPromise,
+    ]);
+
+    
+    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
+    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
+    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
+    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    
+    return {
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    };
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to card data.');
   }
 }
