f3e08342a9d1ca3cf0b601d920675237b0e4dfd9
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-17 11:11:05 +0200

    Query for filltered invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,7 +2,7 @@ import prisma from "@/prisma/db";
 import { Customers } from "@prisma/client";
 import { v4 as uuidv4 } from "uuid";
 import { sql } from "@vercel/postgres";
-import { LatestInvoice, LatestInvoiceRaw } from "./definitions";
+import { LatestInvoice, LatestInvoiceRaw, InvoicesTable } from "./definitions";
 import { formatCurrency } from "./utils";
 
 // applied SQL query Logics from https://nextjs.org/learn tutorial
@@ -85,5 +85,40 @@ export async function fetchCardData() {
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to card data.");
+  }
+}
+
+const ITEMS_PER_PAGE = 6;
+export async function fetchFilteredInvoices(
+  query: string,
+  currentPage: number,
+) {
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+  try {
+    const invoices = await sql<InvoicesTable>`
+      SELECT
+        invoices.id,
+        invoices.amount,
+        invoices.date,
+        invoices.status,
+        customers.name,
+        customers.email,
+        customers.image_url
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      WHERE
+        customers.name ILIKE ${`%${query}%`} OR
+        customers.email ILIKE ${`%${query}%`} OR
+        invoices.amount::text ILIKE ${`%${query}%`} OR
+        invoices.date::text ILIKE ${`%${query}%`} OR
+        invoices.status ILIKE ${`%${query}%`}
+      ORDER BY invoices.date DESC
+      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
+    `;
+
+    return invoices.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoices.');
   }
 }
