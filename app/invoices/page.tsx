4519e535c95c3da063439011bd23a11d630fb675
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-17 10:34:57 +0200

    Invoices table rows

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+
 
 export default function Invoices() {
   return (
-    <div>Invoice</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col" className="px-3 py-5 font-medium">
+              Customers
+            </th>
+            <th scope="col" className="px-3 py-5 font-medium">
+              Email
+            </th>
+            <th scope="col" className="px-3 py-5 font-medium">
+              Amount
+            </th>
+            <th scope="col" className="px-3 py-5 font-medium">
+              Date
+            </th>
+            <th scope="col" className="px-3 py-5 font-medium">
+              Status
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
   )
 }
