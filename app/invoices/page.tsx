324eb443961abfca7edf02db2f163dcf867a5d42
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-18 16:07:11 +0200

    Invoice search bar

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -1,6 +1,7 @@
 import Table from "../ui/invoices/table";
-import { useSearchParams } from 'next/navigation';
-
+import { useSearchParams } from "next/navigation";
+import { lusitana } from "../ui/fonts";
+import SearchBar from "../ui/invoices/search";
 export default async function Page({
   searchParams,
 }: {
@@ -9,11 +10,17 @@ export default async function Page({
     page?: string;
   };
 }) {
-  const query = searchParams?.query || '';
+  const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-  
+
   return (
-    <div>
+    <div className="w-full">
+      <div className="p-3 mt-6 m-5">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="p-3 m-5">
+        <SearchBar/>
+      </div>
       <Table query={query} currentPage={currentPage} />
     </div>
   );
