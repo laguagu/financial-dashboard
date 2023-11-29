dea1da89c74064b9286acaeb3861ddf039bbb4b2
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-29 11:20:43 +0200

    edit form imports correct form and fetchers invoices by id

--- app/invoices/[id]/edit/page.tsx
+++ app/invoices/[id]/edit/page.tsx
@@ -1,16 +1,28 @@
-import { fetchCustomersDB } from "@/app/lib/data";
-import Form from "@/app/ui/invoices/form";
+import { fetchCustomersDB, fetchInvoiceById } from "@/app/lib/data";
+import Form from "@/app/ui/invoices/edit-form";
 import React from "react";
+import { notFound } from "next/navigation";
+
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const customers = await fetchCustomersDB();
   const id = params.id
-  console.log("Site id", id);
+  const customers = await fetchCustomersDB();
+  const invoice = await fetchInvoiceById(id)
+
+  if (!invoice) {
+    notFound();
+  }
+  
+  // const [invoice,customers] = await Promise.all([
+  //   fetchInvoiceById(id),
+  //   fetchCustomersDB
+  // ])
+  
   
   return (
     <main className="p-3 mt-6 m-5">
       <h1 className="text-lg mb-5">Invoices</h1>
-      <Form customers={customers} />
+      <Form customers={customers} invoice={invoice}/>
     </main>
   );
 }
