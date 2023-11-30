fed13d43fd2781cc8ac86511114ada06bae038ee
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-30 15:10:30 +0200

    Search params and query for searchBar

--- app/ui/invoices/search.tsx
+++ app/ui/invoices/search.tsx
@@ -1,10 +1,23 @@
 'use client';
 import React from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function SearchBar() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    console.log(params.toString());
+    
+    replace(`${pathname}?${params.toString()}`);  
   }
 
     return (
@@ -18,6 +31,7 @@ export default function SearchBar() {
             onChange={(e) => {
               handleSearch(e.target.value)
             }}
+            defaultValue={searchParams.get('query')?.toString()}
           />
           <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
         </div>
