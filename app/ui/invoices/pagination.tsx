536d3f2d3fcbed5cdca32c0754f9bea4011120c8
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-01 11:54:02 +0200

    Pagination buttons for prev,next pages

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,55 +1,83 @@
 "use client";
+import clsx from "clsx";
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 
-export default function Pagination({totalPages}: {totalPages:number} ) {
+export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
-  
+  const currentPage = Number(searchParams.get("page")) || 1;
+
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
-    params.set('page', pageNumber.toString());
+    params.set("page", pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
-  
-  console.log(createPageURL(currentPage));
-  
+
+  console.log(createPageURL(currentPage - 1));
 
   return (
     <>
       <nav aria-label="Page navigation">
         <ul className="inline-flex">
           <li>
-            <Link
-            className="h-10 px-5 py-2 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-blue-600 rounded-l-lg focus:shadow-outline hover:bg-indigo-100"
-            href={"/"}
-            >
-              Prev
-            </Link>
+            <PaginationButton
+              href={createPageURL(currentPage - 1)}
+              name={"Prev"}
+            />
           </li>
           <li>
-            <Link href={"/"} className="h-10 px-5 py-2 text-blue-500 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline">
+            <Link
+              href={"/"}
+              className="h-10 px-5 py-2 text-blue-500 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline"
+            >
               1
             </Link>
           </li>
           <li>
-            <Link href={"/"} className="h-10 px-5 py-2 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline hover:bg-indigo-100">
+            <Link
+              href={"/"}
+              className="h-10 px-5 py-2 text-blue-500 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline hover:bg-indigo-100"
+            >
               2
             </Link>
           </li>
           <li>
-            <Link href={"/"} className="h-10 px-5 py-2 text-white transition-colors duration-150 bg-blue-500 border border-r-0 border-blue-600 focus:shadow-outline">
+            <Link
+              href={"/"}
+              className="h-10 px-5 py-2 text-white transition-colors duration-150 bg-blue-500 border border-r-0 border-blue-600 focus:shadow-outline"
+            >
               3
             </Link>
           </li>
           <li>
-            <Link href={"/"} className="h-10 px-5 py-2 text-indigo-600 transition-colors duration-150 bg-white border border-blue-600 rounded-r-lg focus:shadow-outline hover:bg-indigo-100">
-              Next
-            </Link>
+          <PaginationButton
+              href={createPageURL(currentPage + 1)}
+              name={"Next"}
+            />
           </li>
         </ul>
       </nav>
     </>
   );
+
+  function PaginationButton({ href, name }: { href: string; name: string }) {
+    const className = clsx(
+      "h-10 px-5 py-2 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline hover:bg-indigo-100",
+      {
+        "rounded-l-lg": name==="Prev",
+        "rounded-r-lg": name==="Next"
+      }
+      );
+    return (
+      <>
+        <Link
+          className={className}
+          href={href}
+        >
+          {name}
+        </Link>
+      </>
+    );
+  }
 }
