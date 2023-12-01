7b1c652b9c199d770831d32edd0b0159efdea92d
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-01 10:44:54 +0200

    Generate page url function

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,6 +4,16 @@ import { usePathname, useSearchParams } from "next/navigation";
 export default function Pagination({totalPages}: {totalPages:number} ) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+  
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+  
+  console.log(createPageURL(currentPage));
+  
 
   return (
     <>
