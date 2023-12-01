1d04c62d15119e7bf23868ebc30d6804f6367e59
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-01 11:17:53 +0200

    Change buttons to Link

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 
 export default function Pagination({totalPages}: {totalPages:number} ) {
@@ -20,29 +21,32 @@ export default function Pagination({totalPages}: {totalPages:number} ) {
       <nav aria-label="Page navigation">
         <ul className="inline-flex">
           <li>
-            <button className="h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-blue-600 rounded-l-lg focus:shadow-outline hover:bg-indigo-100">
+            <Link
+            className="h-10 px-5 py-2 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-blue-600 rounded-l-lg focus:shadow-outline hover:bg-indigo-100"
+            href={"/"}
+            >
               Prev
-            </button>
+            </Link>
           </li>
           <li>
-            <button className="h-10 px-5 text-blue-500 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline">
+            <Link href={"/"} className="h-10 px-5 py-2 text-blue-500 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline">
               1
-            </button>
+            </Link>
           </li>
           <li>
-            <button className="h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline hover:bg-indigo-100">
+            <Link href={"/"} className="h-10 px-5 py-2 text-indigo-600 transition-colors duration-150 bg-white border border-r-0 border-blue-600 focus:shadow-outline hover:bg-indigo-100">
               2
-            </button>
+            </Link>
           </li>
           <li>
-            <button className="h-10 px-5 text-white transition-colors duration-150 bg-blue-500 border border-r-0 border-blue-600 focus:shadow-outline">
+            <Link href={"/"} className="h-10 px-5 py-2 text-white transition-colors duration-150 bg-blue-500 border border-r-0 border-blue-600 focus:shadow-outline">
               3
-            </button>
+            </Link>
           </li>
           <li>
-            <button className="h-10 px-5 text-indigo-600 transition-colors duration-150 bg-white border border-blue-600 rounded-r-lg focus:shadow-outline hover:bg-indigo-100">
+            <Link href={"/"} className="h-10 px-5 py-2 text-indigo-600 transition-colors duration-150 bg-white border border-blue-600 rounded-r-lg focus:shadow-outline hover:bg-indigo-100">
               Next
-            </button>
+            </Link>
           </li>
         </ul>
       </nav>
