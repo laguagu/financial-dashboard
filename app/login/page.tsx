99b3cf667732360b190c4a14c6d93e3b5c71a8f9
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-11 16:08:26 +0200

    Create login page

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../ui/login-form";
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div>
+        <h1>Login Page</h1>
+        <LoginForm />
+      </div>
+    </main>
+  );
+}
