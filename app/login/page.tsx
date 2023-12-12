a334a6ffa38846206f616743cd266d94b79a6fad
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-12 17:29:36 +0200

    Dummy commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,6 @@ import { authenticate } from "@/app/lib/actions";
 export default function LoginPage() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
-  
   return (
     <form action={dispatch}>
       <main className="flex items-center justify-center md:h-screen">
