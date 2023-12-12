07167ef50ffe3b0db527ec86fe4f009ba1ec349c
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-12 11:13:32 +0200

    Add auth and form action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 import LoginForm from "../ui/login-form";
+import { useFormState, useFormStatus } from "react-dom";
+import { authenticate } from "@/app/lib/actions";
 
 export default function LoginPage() {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div>
-        <h1>Login Page</h1>
-        <LoginForm />
-      </div>
-    </main>
+    <form action={dispatch}>
+      <main className="flex items-center justify-center md:h-screen">
+        <div>
+          <h1>Login Page</h1>
+          <LoginForm />
+        </div>
+      </main>
+    </form>
   );
 }
+ 
