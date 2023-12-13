34830f39ccd1e60a0d0469768f262fa5aa9468ff
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-13 10:10:26 +0200

    Next.js authenticate default login true

--- auth.config.ts
+++ auth.config.ts
@@ -6,13 +6,13 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
+      const isLoggedIn = !!auth?.user || true;
+      const isOnHomePage = nextUrl.pathname.startsWith('/');
+      if (isOnHomePage) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/', nextUrl));
       }
       return true;
     },
