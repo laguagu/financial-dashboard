3241a6da737e65468338b839639d4fd989d31a81
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-15 11:11:37 +0200

    Login form and page template

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,6 @@ export default function LoginPage() {
     <form action={dispatch}>
       <main className="flex items-center justify-center md:h-screen">
         <div>
-          <h1>Login Page</h1>
           <LoginForm />
         </div>
       </main>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,5 +1,40 @@
 export default function LoginForm() {
   return (
-    <div>LoginForm</div>
-  )
+    <div className="w-full">
+      <form className="bg-gray-50 px-6 pb-4 pt-8">
+        <div>
+          <label
+            className="block mb-3 text-base text-gray-900 font-medium"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Enter your email address"
+            required
+          />
+        </div>
+        <div className="mt-4">
+          <label
+            className="block mb-3 text-base text-gray-900 font-medium"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            required
+            minLength={4}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
