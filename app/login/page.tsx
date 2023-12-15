718bf1991635c04bd9dbdbcf0fad6283caa4e8f0
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-15 16:20:16 +0200

    Login form imports fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,14 @@
 "use client";
 import LoginForm from "../ui/login-form";
-import { useFormState, useFormStatus } from "react-dom";
-import { authenticate } from "@/app/lib/actions";
 
 export default function LoginPage() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-    <form action={dispatch}>
       <main className="flex items-center justify-center md:h-screen">
         <div>
           <LoginForm />
         </div>
       </main>
-    </form>
   );
 }
  
