ff5bb3f1d5c62261fafb73defa95db6dc17b7af7
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-18 10:55:41 +0200

    add Useformstate to login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,10 +1,14 @@
-import { useFormStatus } from "react-dom";
-import LoggedIn from "./user-logged";
+'use client';
+import { useFormState, useFormStatus } from "react-dom";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { authenticate } from "../lib/actions";
 
 export default function LoginForm() {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+  
   return (
     <div className="w-full">
-      <form className="bg-gray-50 px-6 pb-4 pt-8">
+      <form className="bg-gray-50 px-6 pb-4 pt-8 space-y-3" action={dispatch}>
         <div className="mb-3 block font-medium text-gray-900">
           <h1 className="text-2xl mb-3">Please log in to continue.</h1>
         </div>
@@ -54,7 +58,7 @@ function LoginButton() {
       className="w-full mt-4 flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
       aria-disabled={pending}
     >
-      Log in
+    Log in <ArrowRightIcon className="h-5 w-5 ml-auto" />
     </button>
   );
 }
