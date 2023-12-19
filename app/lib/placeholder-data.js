bbd96eaee23bb7a30fde0c0c60bfcb953bd860f7
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-19 12:32:26 +0200

    app/lib/placeholder-data.js

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -3,9 +3,15 @@
 
 const users = [
     {
-      id: '410544b2-4001-4271-9855-fec4b6a6442a',
+      id: '1',
       name: 'User',
       email: 'user@nextmail.com',
+      password: '123456',
+    },
+    {
+      id: '2',
+      name: 'User2',
+      email: 'user2@nextmail.com',
       password: '123456',
     },
   ];
