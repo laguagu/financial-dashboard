ddada79e94f241ee081a4b0274c958fef41b4c12
laguagu <49702497+laguagu@users.noreply.github.com>
2023-12-28 16:36:28 +0200

    Add loginpage logo and welcome text

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,17 @@
 "use client";
+import { CreditCardIcon } from "@heroicons/react/24/outline";
 import LoginForm from "../ui/login-form";
 
 export default function LoginPage() {
-
   return (
-      <main className="flex items-center justify-center md:h-screen">
-        <div>
-          <LoginForm />
+    <main className="flex items-center justify-center md:h-screen">
+      <div>
+        <div className="flex flex-col items-center justify-center">
+        <p className="text-center text-xl font-medium">Hello user!</p>
+          <CreditCardIcon className="h-20 w-20 mb-3 text-blue-400 mt-10" />
         </div>
-      </main>
+        <LoginForm />
+      </div>
+    </main>
   );
 }
- 
