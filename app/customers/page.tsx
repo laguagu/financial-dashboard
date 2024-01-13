a2facfb3b6d8150e3c3b68f748614b965d6fdc1d
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-13 13:39:11 +0200

    Add new customers route

---
+++ app/customers/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
+export default function CustomersPage() {
+    return (
+        <div>
+            <h1>Customers Page</h1>
+        </div>
+    );
+};
+
