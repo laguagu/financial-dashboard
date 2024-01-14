87b5b0ef392fc90fb899d7d1bb5574ba57db4f5b
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-14 11:52:13 +0200

    add Statistic information for customers

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,14 +1,28 @@
 import { Metadata } from "next";
+import { UserIcon } from "@heroicons/react/24/outline";
 
 export const metadata: Metadata = {
-    title: 'Customers',
+  title: "Customers",
 };
 
 export default function CustomersPage() {
-    return (
-        <div>
-            <h1>Customers Page</h1>
+  return (
+    <div className="bg-gray-500 flex items-center justify-around w-full rounded-2xl m-4">
+      <div className="flex items-center m-5">
+        <div className="bg-slate-400 w-10 h-10 rounded-2xl">
+          <UserIcon className="w-10 h-10 text-green-400"/>
         </div>
-    );
-};
+        <p>Total Customers</p>
+      </div>
+      <div className="border-l-2 border-white h-8"></div> {/* Lisää pystyviiva */}
+      <div>
+        <p>Total Customers</p>
+      </div>
+      <div className="border-l-2 border-white h-8"></div> {/* Lisää pystyviiva */}
+      <div>
+        <p>Total Customers</p>
+      </div>
+    </div>
+  );
+}
 
