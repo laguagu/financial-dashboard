6ad703ecb6e27031f590349ef1f2ad9268e7f2ab
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-15 11:40:05 +0200

    Fix react skeleton height and count

--- app/(overview)/loading.tsx
+++ app/(overview)/loading.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function Loading() {
-  return (
-    <div>Loading</div>
-  )
+  return <Skeleton height={100} count={7} />;
 }
