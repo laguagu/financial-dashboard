977504e773a7f0c8b6af814e8a93453e91b26fd8
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-24 14:12:44 +0200

    Customers page styles+fonts

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,28 +1,59 @@
 import { Metadata } from "next";
-import { UserIcon } from "@heroicons/react/24/outline";
-
+import { UserIcon, ArrowUpIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
 export const metadata: Metadata = {
   title: "Customers",
 };
 
 export default function CustomersPage() {
   return (
-    <div className="bg-gray-500 flex items-center justify-around w-full rounded-2xl m-4">
-      <div className="flex items-center m-5">
-        <div className="bg-slate-400 w-10 h-10 rounded-2xl">
-          <UserIcon className="w-10 h-10 text-green-400"/>
+    <div className="bg-gray-100 flex items-center justify-around rounded-2xl m-10">
+      <div className="flex items-center m-5 p-3">
+        <div className="flex items-center justify-center bg-green-100 w-16 h-16 rounded-full mr-2">
+          <UserIcon className="w-12 h-12 text-green-400" />
         </div>
-        <p>Total Customers</p>
+        <div className="space-y-1 ml-4">
+          <p className="text-gray-300">Total Customers</p>
+          <p className={`${lusitana.className} text-4xl`}>5,423</p>
+          <div className="flex items-center justify-center">
+            <ArrowUpIcon className="w-4 h-4 text-green-500" />
+            <span className="text-green-500">+16%</span>
+            <p className="ml-1 text-sm font-medium">This month</p>
+          </div>
+        </div>
       </div>
-      <div className="border-l-2 border-white h-8"></div> {/* Lisää pystyviiva */}
-      <div>
-        <p>Total Customers</p>
+      <div className="border-l-2 border-white h-24"></div>{" "}
+      {/* Lisää pystyviiva */}
+      <div className="flex items-center m-5 p-3">
+        <div className="flex items-center justify-center bg-green-100 w-16 h-16 rounded-full mr-2">
+          <UserIcon className="w-12 h-12 text-green-400" />
+        </div>
+        <div className="space-y-1 ml-4">
+          <p className="text-gray-300">Total Customers</p>
+          <p className={`${lusitana.className} text-4xl`}>5,423</p>
+          <div className="flex items-center justify-center">
+            <ArrowUpIcon className="w-4 h-4 text-green-500" />
+            <span className="text-green-500">+16%</span>
+            <p className="ml-1 text-sm font-medium">This month</p>
+          </div>
+        </div>
       </div>
-      <div className="border-l-2 border-white h-8"></div> {/* Lisää pystyviiva */}
-      <div>
-        <p>Total Customers</p>
+      <div className="border-l-2 border-white h-24"></div>{" "}
+      {/* Lisää pystyviiva */}
+      <div className="flex items-center m-5 p-3">
+        <div className="flex items-center justify-center bg-green-100 w-16 h-16 rounded-full mr-2">
+          <UserIcon className="w-12 h-12 text-green-400" />
+        </div>
+        <div className="space-y-1 ml-4">
+          <p className="text-gray-300">Total Customers</p>
+          <p className={`${lusitana.className} text-4xl`}>5,423</p>
+          <div className="flex items-center justify-center">
+            <ArrowUpIcon className="w-4 h-4 text-green-500" />
+            <span className="text-green-500">+16%</span>
+            <p className="ml-1 text-sm font-medium">This month</p>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
