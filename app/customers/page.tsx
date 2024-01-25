ca0f9b5d24588ef6966c76769e5fa87b473b592b
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-25 17:23:46 +0200

    Add shadcn avatar

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import CustomerCard from "../ui/customers/customer-card";
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 export const metadata: Metadata = {
   title: "Customers",
 };
@@ -8,7 +8,15 @@ export const metadata: Metadata = {
 export default function CustomersPage() {
   return (
     <div>
-      <CustomerCard />
+      <div className="flex flex-col items-end">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+      <div>
+        <CustomerCard />
+      </div>
     </div>
   );
 }
