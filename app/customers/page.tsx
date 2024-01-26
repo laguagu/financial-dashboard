9f799c51e5647736929f77da01a743bd5ed99f96
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-26 16:29:22 +0200

    Add theme mode toggle button

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,18 +1,20 @@
 import { Metadata } from "next";
 import CustomerCard from "../ui/customers/customer-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ModeToggle } from "../ui/toggle-theme";
 export const metadata: Metadata = {
   title: "Customers",
 };
 
 export default function CustomersPage() {
   return (
-    <div>
-      <div className="flex flex-col items-end">
-        <Avatar>
+    <div className="">
+      <div className="flex flex-row justify-end mt-2 mr-2">
+        <Avatar className="mr-4">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
+        <ModeToggle/>
       </div>
       <div>
         <CustomerCard />
