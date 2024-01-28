63d8448ed727d4cd534aa647084f592bf244244d
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-28 21:36:36 +0200

    Add searchbar and users welcome text

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -2,19 +2,29 @@ import { Metadata } from "next";
 import CustomerCard from "../ui/customers/customer-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ModeToggle } from "../ui/toggle-theme";
+import CustomerSearch from "../ui/customers/searchbar";
+
 export const metadata: Metadata = {
   title: "Customers",
 };
 
 export default function CustomersPage() {
   return (
-    <div className="">
-      <div className="flex flex-row justify-end mt-2 mr-2">
+    <div>
+      <div className="flex flex-row justify-end mt-2 mr-2 mb-4">
         <Avatar className="mr-4">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-        <ModeToggle/>
+        <ModeToggle />
+      </div>
+      <div className="grid grid-cols-2 mx-9">
+        <div className="col-span-1 flex items-center justify-center">
+          <p>Hei Matti! 👋</p>
+        </div>
+        <div className="col-span-1 flex items-center justify-center">
+          <CustomerSearch />
+        </div>
       </div>
       <div>
         <CustomerCard />
