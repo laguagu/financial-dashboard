8fa143bc520f31a30e4a0aa9816734627edc1071
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-29 20:23:11 +0200

    feat: Map members details to table rows

--- app/ui/customers/all-customers.tsx
+++ app/ui/customers/all-customers.tsx
@@ -1,13 +1,18 @@
 import { DatePickerDemo } from "./datepicker";
+import { fetchMembersDB } from "@/app/lib/data";
+import { Members } from "@/app/lib/definitions";
 
-function AllCustomersCard() {
+async function AllCustomersCard() {
+  const members: Members[] = await fetchMembersDB();
+  console.log(members);
+
   return (
     <div className="flex m-10 ">
       <div className="flex flex-col flex-grow bg-gray-100 w-full items-center mx-auto p-6 border rounded-2xl">
         <div className="flex justify-between w-full items-center">
           <div>
             <p className="text-2xl font-bold">All Customers</p>
-            <p className="text-green-500 text-sm">Active Members</p>
+            <p className="text-green-500 text-sm">Active Paid Members</p>
           </div>
           <div>
             <DatePickerDemo />
@@ -16,23 +21,37 @@ function AllCustomersCard() {
         <table className="w-full mt-8">
           <thead className="text-stone-400 text-left">
             <tr className="w-full px-5 py-3">
-              <th className="flex-1 font-medium">Customer Name</th>
-              <th className="flex-1 font-medium">Company</th>
-              <th className="flex-1 font-medium">Phone Number</th>
-              <th className="flex-1 font-medium">Email</th>
-              <th className="flex-1 font-medium">Country</th>
-              <th className="flex-1 font-medium">Status</th>
+              <th scope="col" className="flex-1 font-medium">
+                Customer Name
+              </th>
+              <th scope="col" className="flex-1 font-medium">
+                Company
+              </th>
+              <th scope="col" className="flex-1 font-medium">
+                Phone Number
+              </th>
+              <th scope="col" className="flex-1 font-medium">
+                Email
+              </th>
+              <th scope="col" className="flex-1 font-medium">
+                Country
+              </th>
+              <th scope="col" className="flex-1 font-medium">
+                Status
+              </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="w-full px-5 py-3">
-              <td className="flex-1 font-medium">Matti Nykänen</td>
-              <td className="flex-1 font-medium">Microsoft</td>
-              <td className="flex-1 font-medium">04012304534</td>
-              <td className="flex-1 font-medium">Matti@gmail.com</td>
-              <td className="flex-1 font-medium">United States</td>
-              <td className="flex-1 font-medium">Active</td>
-            </tr>
+            {members.map((member) => (
+              <tr key={member.id} className="w-full px-5 py-3">
+                <td className="flex-1 font-medium py-3">{member.name}</td>
+                <td className="flex-1 font-medium py-3">{member.company}</td>
+                <td className="flex-1 font-medium py-3">{member.phonenumber}</td>
+                <td className="flex-1 font-medium py-3">{member.email}</td>
+                <td className="flex-1 font-medium py-3">{member.country}</td>
+                <td className="flex-1 font-medium py-3">{member.status}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
