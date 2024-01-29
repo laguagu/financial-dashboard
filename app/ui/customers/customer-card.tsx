364e49c08060a7be759e7e5d99e0b8447b64c32a
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-29 10:15:28 +0200

    Edit card texts and icons

--- app/ui/customers/customer-card.tsx
+++ app/ui/customers/customer-card.tsx
@@ -1,9 +1,13 @@
-import { UserIcon, ArrowUpIcon } from "@heroicons/react/24/outline";
+import {
+  UserIcon,
+  ArrowUpIcon,
+  ArrowDownIcon,
+} from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
 
 function CustomerCard() {
   return (
-    <div className="bg-gray-100 flex items-center justify-around rounded-2xl m-10">
+    <div className="bg-gray-100 flex items-center justify-around rounded-2xl m-10 align-middle">
       <div className="flex items-center m-5 p-3">
         <div className="flex items-center justify-center bg-green-100 w-16 h-16 rounded-full mr-2">
           <UserIcon className="w-12 h-12 text-green-400" />
@@ -25,11 +29,11 @@ function CustomerCard() {
           <UserIcon className="w-12 h-12 text-green-400" />
         </div>
         <div className="space-y-1 ml-4">
-          <p className="text-stone-400">Total Customers</p>
-          <p className={`${lusitana.className} text-4xl`}>5,423</p>
+          <p className="text-stone-400">Members</p>
+          <p className={`${lusitana.className} text-4xl`}>1,893</p>
           <div className="flex items-center justify-center">
-            <ArrowUpIcon className="w-4 h-4 text-green-500" />
-            <span className="text-green-500">+16%</span>
+            <ArrowDownIcon className="w-4 h-4 text-red-500" />
+            <span className="text-red-500">+16%</span>
             <p className="ml-1 text-sm font-medium">This month</p>
           </div>
         </div>
@@ -41,13 +45,8 @@ function CustomerCard() {
           <UserIcon className="w-12 h-12 text-green-400" />
         </div>
         <div className="space-y-1 ml-4">
-          <p className="text-stone-400">Total Customers</p>
-          <p className={`${lusitana.className} text-4xl`}>5,423</p>
-          <div className="flex items-center justify-center">
-            <ArrowUpIcon className="w-4 h-4 text-green-500" />
-            <span className="text-green-500">+16%</span>
-            <p className="ml-1 text-sm font-medium">This month</p>
-          </div>
+          <p className="text-stone-400">Active Now</p>
+          <p className={`${lusitana.className} text-4xl`}>189</p>
         </div>
       </div>
     </div>
