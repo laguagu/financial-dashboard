1981210514938deae33a696444b95936ae7f4f44
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-30 13:00:53 +0200

    style:Add style changes to table

--- app/ui/customers/all-customers.tsx
+++ app/ui/customers/all-customers.tsx
@@ -1,13 +1,14 @@
 import { DatePickerDemo } from "./datepicker";
 import { fetchMembersDB } from "@/app/lib/data";
 import { Members } from "@/app/lib/definitions";
+import clsx from "clsx";
 
 async function AllCustomersCard() {
   const members: Members[] = await fetchMembersDB();
   console.log(members);
 
   return (
-    <div className="flex m-10 ">
+    <div className="flex m-10">
       <div className="flex flex-col flex-grow bg-gray-100 w-full items-center mx-auto p-6 border rounded-2xl">
         <div className="flex justify-between w-full items-center">
           <div>
@@ -18,9 +19,9 @@ async function AllCustomersCard() {
             <DatePickerDemo />
           </div>
         </div>
-        <table className="w-full mt-8">
-          <thead className="text-stone-400 text-left">
-            <tr className="w-full px-5 py-3">
+        <table className="w-full mt-10">
+          <thead className="text-neutral-300 text-left border-b-2 mb-5">
+            <tr>
               <th scope="col" className="flex-1 font-medium">
                 Customer Name
               </th>
@@ -36,24 +37,40 @@ async function AllCustomersCard() {
               <th scope="col" className="flex-1 font-medium">
                 Country
               </th>
-              <th scope="col" className="flex-1 font-medium">
+              <th scope="col" className="flex-1 font-medium text-center">
                 Status
               </th>
             </tr>
           </thead>
           <tbody>
             {members.map((member) => (
-              <tr key={member.id} className="w-full px-5 py-3">
-                <td className="flex-1 font-medium py-3">{member.name}</td>
-                <td className="flex-1 font-medium py-3">{member.company}</td>
-                <td className="flex-1 font-medium py-3">{member.phonenumber}</td>
-                <td className="flex-1 font-medium py-3">{member.email}</td>
-                <td className="flex-1 font-medium py-3">{member.country}</td>
-                <td className="flex-1 font-medium py-3">{member.status}</td>
+              <tr key={member.id}>
+                <td className="flex-1 py-3 font-semibold">{member.name}</td>
+                <td className="flex-1 py-3 font-semibold">{member.company}</td>
+                <td className="flex-1 py-3 font-semibold">
+                  {member.phonenumber}
+                </td>
+                <td className="flex-1 py-3 font-semibold">{member.email}</td>
+                <td className="flex-1 py-3 font-semibold">{member.country}</td>
+                <td
+                  className={clsx("flex-1 py-3 font-semibold", {
+                    "text-green-400 text-center p-4":
+                      member.status === "Active",
+                    "text-red-400 text-center p-4":
+                      member.status === "Inactive",
+                  })}
+                >
+                  {member.status}
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
+        <div className="flex justify-start mt-4 w-full border-t pt-4">
+          <p className="text-gray-500 text-sm">
+            Showind data 1 to 8 of 256k entries
+          </p>
+        </div>
       </div>
     </div>
   );
