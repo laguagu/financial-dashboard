7cf2485079ee0ad36056d7105b1f18dc224c7cce
laguagu <49702497+laguagu@users.noreply.github.com>
2024-01-30 19:31:05 +0200

    fix:Trying optimize img

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import React from "react";
 import NavLinks from "./navlinks";
 import { PowerIcon } from "@heroicons/react/24/outline";
-import { CreditCardIcon } from "@heroicons/react/20/solid";
 import { signOut } from "@/auth";
 import logo from "@/public/fundfleet.png"
 import Image from "next/image";
@@ -14,8 +13,11 @@ export default function Sidenav() {
         href="/"
         className="mb-2 flex rounded-md  p-4 md:h-28 bg-zinc-100 items-center justify-center "
       >
-        <CreditCardIcon className="h-20 w-20 mb-3 text-blue-400 mt-10" />
-        <Image alt="asd" src={logo}/>
+        <Image
+         alt="asd" 
+         src={logo} 
+         priority
+         />
       </Link>
       <NavLinks />
       <div className="mt-auto whitespace-nowrap">
