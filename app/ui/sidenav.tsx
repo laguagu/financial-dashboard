5df35a1319535a68f0688e7c391fb2ddc452f6c4
laguagu <49702497+laguagu@users.noreply.github.com>
2024-02-04 19:34:36 +0200

    fix: Sign out button alignmemnt down the sidenav

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -8,10 +8,10 @@ import Image from "next/image";
 
 export default function Sidenav() {
   return (
-    <nav className="flex-row md:flex-col px-3 md:px-2 py-4 text-zinc-400 md:w-44 w-40 bg-slate-50 min-h-screen">
+    <nav className="flex flex-row md:flex-col px-3 md:px-2 py-4 text-zinc-400 md:w-44 w-40 bg-slate-50 min-h-screen">
       <Link
         href="/"
-        className="mb-2 flex rounded-md  p-4 md:h-28 bg-zinc-100 items-center justify-center "
+        className="mb-2 flex rounded-md p-4 md:h-28 bg-zinc-100 items-center justify-center "
       >
         <Image
          alt="asd" 
@@ -29,7 +29,7 @@ export default function Sidenav() {
         >
           <button className="flex items-center gap-2 p-3 hover:bg-sky-100 hover:text-blue-600 rounded-lg hover:font-semibold transition-colors duration-300">
             <PowerIcon className="w-6" />
-            <div className="md:block text-center ">Sign Out</div>
+            <div className="md:block text-center">Sign Out</div>
           </button>
         </form>
       </div>
